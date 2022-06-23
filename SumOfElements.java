dbad4b0d177824ccc07c3d925617d3f5c335c6a3
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-06-23 23:40:51 +0530

    Create SumOfElements.java

---
+++ SumOfElements.java
@@ -0,0 +1,29 @@
+/*
+Write a program to accept N numbers in an array and display sum of all elements at even positions.............................................................................
+*/
+import java.util.Scanner;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter size");
+		n=s.nextInt();
+		int arr[]=new int[n];
+		System.out.println("enter elements:");
+		for(int i=0;i<n;i++) {
+			arr[i]=s.nextInt();
+		}
+		int sum=0;
+		for(int i=0;i<n;i++) {
+			if(i%2==0) {
+				sum=sum+arr[i];
+			}
+		}
+		System.out.println("sum of elements at even position is:"+sum);
+
+	}
+
+}
